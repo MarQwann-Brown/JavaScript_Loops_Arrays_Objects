8fa10a69648bf4aabf0b7363c405ad364916b0f7
MarQwann Brown <marqwann@gmail.com>
2017-05-15 00:24:47 -0400

    Work on the userAnswers loop
    Loop is now working but still need the correct text format

--- Section_2_Arrays/challenge01/js/quiz.js
+++ Section_2_Arrays/challenge01/js/quiz.js
@@ -8,14 +8,24 @@ var qa = [
     ['What is 10 x 10?', '100']
 ];
 var i;
+var questionNumber;
 var userAnswer = [];
+
 function print(message) {
     document.write(message);
 }
-
+//asks questions and stores the answers in 'userAnswer'
 for (i = 0; i < qa.length; i+=1){
-    prompt(qa[i][0]);
-    print(qa[i]);
+	questionNumber = i+1;
+    userAnswer.push(prompt(qa[i][0]));
+	
+	if(userAnswer[i] === qa[i][1]){
+		print('You correctly answered question number ' + questionNumber + ': '+ '"' + qa[i][0] + '"' + '<br>');
+	} else{
+		print('Your answer for question number ' + questionNumber +  ' was incorrect. The correct answer is ' + qa[i][1] + '<br>');
+	}
 
+	//Your answer for question number 1: "934" was incorrect, the correct answer is 50
+	
+//print(userAnswer);
 }
-
