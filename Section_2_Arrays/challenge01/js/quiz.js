d95bf54b8c52e7bed51feb0a9f1fc152699b3856
MarQwann Brown <marqwann@gmail.com>
2017-05-15 23:56:38 -0400

    more work on the challenge

--- Section_2_Arrays/challenge01/js/quiz.js
+++ Section_2_Arrays/challenge01/js/quiz.js
@@ -2,30 +2,42 @@
 //ask series of 3 questions and evaluate answer
 //keep track of questions answered
 //keep track of questions answered wrong -- "You answered *userAnswer*, the correct answer is *correctAnswer*"
-var qa = [
+var questionAnswerArray = [
     ['How many states are there in the USA?', '50'],
     ['How many letters are there in the alphabet?', '26'],
     ['What is 10 x 10?', '100']
 ];
 var i;
 var questionNumber;
-var userAnswer = [];
+var userAnswerArray = [];
+var questionsCorrect;
+var questionsIncorrect;
 
 function print(message) {
     document.write(message);
 }
+
+console.log(questionAnswerArray);
+print('<b>' + 'You got these answers correct:' + '</b>' + '<br>');
 //asks questions and stores the answers in 'userAnswer'
-for (i = 0; i < qa.length; i+=1){
+for (i = 0; i < questionAnswerArray.length; i+=1){
 	questionNumber = i+1;
-    userAnswer.push(prompt(qa[i][0]));
+    userAnswerArray.push(prompt(questionAnswerArray[i][0]));
+	userAnswerArray.unshift(questionAnswerArray[i][0]);
+}
+
+console.log(userAnswerArray);
+print(userAnswerArray);
+//for (i = 0; i < userAnswerArray.length; i+=1){
+//	if (userAnswerArray[i] === questionAnswerArray[i][1]){
+//		print('<br>' + qa[i][0] + '<br>');
+//	} else if (userAnswer[i] !== qa[i][1]){
+//		print('<br' + 'You got these questions wrong:' + '<br>');
+//		print('<br>' + qa[i][0] + '<br>');
+//		}
 	
-	if(userAnswer[i] === qa[i][1]){
-		print('You correctly answered question number ' + questionNumber + ': '+ '"' + qa[i][0] + '"' + '<br>');
-	} else{
-		print('Your answer for question number ' + questionNumber +  ' was incorrect. The correct answer is ' + qa[i][1] + '<br>');
-	}
+
 
 	//Your answer for question number 1: "934" was incorrect, the correct answer is 50
 	
 //print(userAnswer);
-}
